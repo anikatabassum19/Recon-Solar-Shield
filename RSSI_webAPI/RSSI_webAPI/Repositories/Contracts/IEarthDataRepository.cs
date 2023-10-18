3f5984a72a3c34fac59e75827e1d95041fb7837b
Anika Tabassum Orchi <58374595+anikatabassum19@users.noreply.github.com>
2023-10-18 06:58:19 +0600

    Delete RSSI_webAPI directory

--- RSSI_webAPI/RSSI_webAPI/Repositories/Contracts/IEarthDataRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using RSSI_webAPI.Models;
-
-namespace RSSI_webAPI.Repositories.Contracts;
-
-public interface IEarthDataRepository
-{
-    Task<GeoMagnetDataModel?> GetGeoMagneticDataFromBGS();
-    Task<GeoMagnetDataModel?> GetGeoMagneticDataFromNCIE();
-}
